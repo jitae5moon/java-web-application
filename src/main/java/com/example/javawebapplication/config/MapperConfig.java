b2d1f92007f2c9c2a6dec7251db041cbdc96da4b
jitae5moon <jitaemoon@knou.ac.kr>
2024-11-02 15:38:49 +0900

    Add `ModelMapper`

---
+++ src/main/java/com/example/javawebapplication/config/MapperConfig.java
@@ -0,0 +1,22 @@
+package com.example.javawebapplication.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        return modelMapper;
+    }
+
+}
