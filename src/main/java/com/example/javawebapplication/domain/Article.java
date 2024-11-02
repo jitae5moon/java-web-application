b8a398e638115603f03db0be9da76a9be6e10759
jitae5moon <jitaemoon@knou.ac.kr>
2024-11-02 13:08:45 +0900

    Add getter/setter, constructor to entity `Article`

--- src/main/java/com/example/javawebapplication/domain/Article.java
+++ src/main/java/com/example/javawebapplication/domain/Article.java
@@ -1,11 +1,15 @@
 package com.example.javawebapplication.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import javax.persistence.*;
 
+@Getter
 @Entity
 public class Article extends AuditingFields {
 
@@ -13,13 +17,25 @@ public class Article extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(length = 500, nullable = false)
     private String title;
 
+    @Setter
     @Column(length = 2000, nullable = false)
     private String content;
 
+    @Setter
     @Column(length = 50, nullable = false)
     private String createdBy;
+
+    protected Article() { }
+
+    public Article(Long id, String title, String content, String createdBy) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdBy = createdBy;
+    }
 
 }
