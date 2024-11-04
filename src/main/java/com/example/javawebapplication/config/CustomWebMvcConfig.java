11b1c4e66c8a6ac026c12ca491a7fb834bcc2387
jitae5moon <jitaemoon@knou.ac.kr>
2024-11-04 20:48:41 +0900

    Add `Swagger` library and config

---
+++ src/main/java/com/example/javawebapplication/config/CustomWebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.example.javawebapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class CustomWebMvcConfig implements WebMvcConfigurer {
+
+    /*
+     * Swagger UI changed static files path
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/");
+    }
+
+}
